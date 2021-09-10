660d1ba0207bde7e3dc7f20557cfe27a4644eedc
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-09-10 20:05:12 +0530

    add members to rtsp connection struct

--- src/connection.rs
+++ src/connection.rs
@@ -3,40 +3,55 @@ use rtp_packet::*;
 use std::io::{BufRead, Read, Write};
 use std::io::{BufReader, BufWriter};
 use std::net::TcpStream;
+use std::time::Duration;
 use std::vec::Vec;
 use utils::*;
 
+use crate::MediaType;
+
 /// an rtsp connection
+#[derive(Debug)]
 pub struct RtspConnection {
     stream: TcpStream,
     writer: BufWriter<TcpStream>,
     reader: BufReader<TcpStream>,
     url: String,
     cseq: i32,
-    session: u64, //TODO is this needed?
+    session: String,
+    audio_track: String,
+    video_track: String,
 }
 
 impl RtspConnection {
     /// creates a new `RtspConnection`
-    pub fn new(url: String) -> Result<RtspConnection, String> {
-        let (server, port) = match parse_rtsp_url(&url) {
+    pub fn new(url: &str) -> Result<RtspConnection, String> {
+        let (server, port) = match parse_rtsp_url(url.into()) {
             Ok((server, port)) => (server, port),
             Err(e) => return Err(e),
         };
 
-        let stream = TcpStream::connect(format!("{}:{}", server, port)).unwrap();
-        //Since self referencing in structs is not supported without TP crates,
-        //we can use try_clone on the TcpStream to create multiple references to the same stream
-        let stream_out = stream.try_clone().unwrap();
-        let stream_in = stream.try_clone().unwrap();
-        Ok(RtspConnection {
-            stream,
-            writer: BufWriter::new(stream_out),
-            reader: BufReader::new(stream_in),
-            url: url.clone(),
-            cseq: 1,
-            session: 0,
-        })
+        println!("connecting to server ... waiting upto 5 secs ...");
+        if let Ok(stream) = TcpStream::connect_timeout(
+            &format!("{}:{}", server, port).parse().unwrap(),
+            Duration::from_secs(5),
+        ) {
+            //Since self referencing in structs is not supported without TP crates,
+            //we can use try_clone on the TcpStream to create multiple references to the same stream
+            let stream_out = stream.try_clone().unwrap();
+            let stream_in = stream.try_clone().unwrap();
+            Ok(RtspConnection {
+                stream,
+                writer: BufWriter::new(stream_out),
+                reader: BufReader::new(stream_in),
+                url: url.into(),
+                cseq: 1,
+                session: String::new(),
+                audio_track: String::new(),
+                video_track: String::new(),
+            })
+        } else {
+            Err("error connecting ...".into())
+        }
     }
 
     /// sends a options command
@@ -45,6 +60,7 @@ impl RtspConnection {
             "OPTIONS {} RTSP/1.0\r\nCSeq: {}\r\n\r\n",
             self.url, self.cseq
         );
+        self.cseq += 1;
         self.send(&command.as_bytes());
     }
 
@@ -54,20 +70,97 @@ impl RtspConnection {
             "DESCRIBE {} RTSP/1.0\r\nCSeq: {}\r\nAccept: application/sdp\r\n\r\n",
             self.url, self.cseq
         );
+        self.cseq += 1;
         self.send(&command.as_bytes());
+
+        let mut line = String::new();
+        self.reader.read_line(&mut line);
+        let mut content_length = 0;
+        let mut session_parsed = false;
+        let mut audio_section = false;
+        let mut content_length_parsed = false;
+        let mut buf = String::new();
+
+        while line != "\r\n" {
+            //TODO create structs to represent RTSP responses
+            if !session_parsed && line.contains("Session") {
+                let v: Vec<&str> = line.split(':').collect();
+                let v: Vec<&str> = v[1].split(';').collect();
+                self.session = v[0].trim_start().parse().unwrap();
+                session_parsed = true;
+            }
+            if !content_length_parsed && line.contains("Content-Length") {
+                let v: Vec<&str> = line.split(':').collect();
+                content_length = v[1].trim().parse().unwrap();
+                content_length_parsed = true;
+            }
+
+            if line.contains("m=audio") {
+                audio_section = true;
+            }
+
+            if line.contains("m=video") {
+                audio_section = false;
+            }
+
+            if line.contains("a=control") {
+                let track = line.split(':').collect::<Vec<&str>>()[1];
+                if track != "*" {
+                    if audio_section {
+                        self.audio_track = track.into();
+                    } else {
+                        self.video_track = track.into();
+                    }
+                }
+            }
+
+            buf.push_str(&line);
+            line.clear();
+            self.reader.read_line(&mut line);
+        }
+
+        let mut vec = vec![0u8; content_length];
+        self.reader.read_exact(&mut vec);
+        buf.push_str(&String::from_utf8(vec).unwrap());
     }
 
     /// sends a setup command
-    pub fn setup(&mut self) {
-        let command = format!("SETUP {}/track1 RTSP/1.0\r\nCSeq: {}\r\nUser-Agent: insight\r\nTransport: RTP/AVP;unicast;interleaved=0-1\r\n\r\n",           self.url, self.cseq
+    pub fn setup(&mut self, media: MediaType) {
+        match media {
+            MediaType::Video => self._setup(self.video_track.clone()),
+            MediaType::Audio => self._setup(self.audio_track.clone()),
+            MediaType::All => {
+                self._setup(self.video_track.clone());
+                self._setup(self.audio_track.clone());
+            }
+        }
+    }
+
+    fn _setup(&mut self, track: String) {
+        let command = format!("SETUP {}/{} RTSP/1.0\r\nCSeq: {}\r\nUser-Agent: insight\r\nTransport: RTP/AVP;unicast;interleaved=0-1\r\n\r\n",           self.url, track, self.cseq
         );
+        self.cseq += 1;
         self.send(&command.as_bytes());
+
+        let mut data = vec![0; 1500];
+        self.reader.read(&mut data);
     }
 
     /// sends a play command with the given session
-    pub fn play(&mut self, session: u64) {
-        let command = format!("PLAY {} RTSP/1.0\r\nCSeq: {}\r\nUser-Agent: insight\r\nSession: {}\r\nRange: npt=0.000-\r\n\r\n", self.url, self.cseq, session);
+    pub fn play(&mut self) {
+        let command = format!("PLAY {} RTSP/1.0\r\nCSeq: {}\r\nUser-Agent: insight\r\nSession: {}\r\nRange: npt=0.000-\r\n\r\n", self.url, self.cseq, self.session);
+        self.cseq += 1;
         self.send(&command.as_bytes());
+
+        let mut data = vec![0; 1500];
+        self.reader.read(&mut data);
+    }
+
+    /// convenience method that performs rtsp handshake (including play) internally
+    pub fn open(&mut self, media: MediaType) {
+        self.describe();
+        self.setup(media);
+        self.play();
     }
 
     /// sends data over the underlying socket
@@ -75,11 +168,6 @@ impl RtspConnection {
         self.writer.write(data).unwrap();
         //Flushing is necessary in order to send the data over the TCP stream
         self.writer.flush().unwrap();
-    }
-
-    /// reads a response for the sent command
-    pub fn read_generic<T: Response>(&mut self, response: &mut T, data: &mut String) {
-        response.read(&mut self.reader, data);
     }
 
     /// reads rtp packets from the incoming stream
@@ -150,8 +238,8 @@ impl RtspConnection {
     }
 
     /// gets session of this connection
-    pub fn get_session(&self) -> u64 {
-        self.session
+    pub fn get_session(&self) -> &str {
+        &self.session
     }
 
     /// gets port of this connection
