4417f17fdb635ddac96d768b9ff9aa73c7c57919
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-09-11 23:44:05 +0530

    fix tests

--- tests/main.rs
+++ tests/main.rs
@@ -1,42 +1,37 @@
 extern crate insight;
 use insight::connection::*;
 use insight::response::*;
-use insight::utils::*;
+use insight::MediaType;
+// use insight::utils::*;
 
 #[test]
 fn test_integration() {
-    let stream = RtspConnection::new(String::from(
-        "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov",
-    ));
+    let stream = RtspConnection::new("rtsp://34.227.104.115/vod/mp4:BigBuckBunny_175k.mov");
     if stream.is_ok() {
         println!("Connected");
     } else {
-        println!("Error connecting to the server");
+        println!("{}", stream.unwrap_err());
+        return;
     }
 
     let mut conn = stream.unwrap();
     conn.options();
     let mut output = String::new();
     println!("waiting to read more...");
-    //conn.read(&mut output);
     let mut options = Options;
-    conn.read_generic(&mut options, &mut output);
     println!("options output \n{}", output);
 
     output.clear();
     conn.describe();
     let mut describe_response = Describe::new();
-    conn.read_generic(&mut describe_response, &mut output);
     println!("describe output:\n{}", output);
 
     output.clear();
-    conn.setup();
-    conn.read_generic(&mut Setup, &mut output);
+    conn.setup(MediaType::Audio);
     println!("setup output:\n{}", output);
 
     output.clear();
-    conn.play(describe_response.get_session());
-    conn.read_generic(&mut Play, &mut output);
+    conn.play();
     println!("play output:\n{}", output);
 
     output.clear();
@@ -44,11 +39,23 @@ fn test_integration() {
     println!("output len {}\n", output.len());
     loop {
         conn.read_server_stream(&mut output);
-        //println!("server stream output:\n{}", std::str::from_utf8(&output).unwrap());
         for c in output.iter() {
             println!("{:x?}", c);
         }
-
-        //break;
     }
+}
+
+#[test]
+fn test_rtsp_start_all_streams() {
+    let mut connection = RtspConnection::new("some url")
+        .unwrap()
+        .open(MediaType::All);
+}
+
+#[test]
+fn test_rtsp_client_chain() {
+    let mut connection = RtspConnection::new("some url").unwrap();
+    connection.describe();
+    connection.setup(MediaType::All);
+    connection.play();
 }
