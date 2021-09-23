9703a6d4eabbf3221173eba7c88d3240300ff896
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-09-23 23:05:58 +0530

    impl conversion from RTPPacket to Vec<u8>

--- src/rtp_packet.rs
+++ src/rtp_packet.rs
@@ -98,3 +98,48 @@ impl From<&[u8]> for RTPPacket {
         }
     }
 }
+
+impl From<RTPPacket> for Vec<u8> {
+    /*
+     *
+        0                   1                   2                   3
+        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |V=2|P|X|  CC   |M|     PT      |       sequence number         |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |                           timestamp                           |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |           synchronization source (SSRC) identifier            |
+       +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+       |            contributing source (CSRC) identifiers             |
+       |                             ....                              |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    */
+    fn from(packet: RTPPacket) -> Self {
+        // TODO abhi: fix the capacity
+        let mut bytes = Vec::with_capacity(packet.payload.len() + 20);
+        let header_byte: u8 = 0x80
+            | if packet.padding { 1 << 5 } else { 0 }
+            | if packet.extension { 1 << 4 } else { 0 }
+            | packet.cc;
+        bytes[0] = header_byte;
+        bytes[1] = if packet.marker { 0x80 } else { 0 } | packet.payload_type;
+        bytes.extend_from_slice(&packet.seq_num.to_be_bytes());
+        bytes.extend_from_slice(&packet.timestamp.to_be_bytes());
+        bytes.extend_from_slice(&packet.ssrc.to_be_bytes());
+        if let Some(csrcs) = packet.csrcs {
+            for quad in csrcs {
+                bytes.extend_from_slice(&quad.to_be_bytes());
+            }
+        }
+        if let Some(profile_specific_ext_hdr_id) = packet.profile_specific_ext_hdr_id {
+            bytes.extend_from_slice(&profile_specific_ext_hdr_id.to_be_bytes());
+        }
+        if let Some(ext_hdr_len) = packet.ext_hdr_len {
+            bytes.extend_from_slice(&ext_hdr_len.to_be_bytes());
+        }
+        bytes.extend(packet.payload);
+
+        bytes
+    }
+}
